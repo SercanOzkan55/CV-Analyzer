28b2a0c7dc20d6fdac4851f21e0ac81ef3166574
SercanOzkan55 <SercanOzkan55@users.noreply.github.com>
2026-05-17 14:24:01 +0300

    Shorten Alembic billing revision id

--- migrations/versions/add_billing_status_to_organizations.py
+++ migrations/versions/add_billing_status_to_organizations.py
@@ -1,6 +1,6 @@
 """add billing_status to organizations
 
-Revision ID: add_billing_status_to_organizations
+Revision ID: add_billing_status_orgs
 Revises: 4bb2cf15d957
 Create Date: 2026-03-04
 """
@@ -9,7 +9,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "add_billing_status_to_organizations"
+revision = "add_billing_status_orgs"
 down_revision = "4bb2cf15d957"
 branch_labels = None
 depends_on = None
